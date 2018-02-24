17e15b630eaaa474610d4f5cf6277dbe8c6b4365
[name] <2523990559@qq.com>
2018-02-24 13:13:21 +0800

    新增笔记和案例

---
+++ src/view/Search/Content/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        这是子组件
+        <div>{{zyChild}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['zyChild'],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+
+</style>
+
--- src/view/Search/index.vue
+++ src/view/Search/index.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>这是搜索</div>
+  <div>
+    <div style="color:red">这是搜索</div>
+    <div>
+      <Content :zyChild="Child"></Content>
+    </div>
+    </div>
 </template>
+<script>
+import Content from '@/view/Search/Content/index'
+export default {
+  components:{
+    Content
+  },
+  data(){
+    return{
+      Child:'我是孩子'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
+
